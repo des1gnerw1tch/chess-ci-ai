2a948568206c527128a0b1024dc2bd35bc731a8e
bobdudeman <59026247+bobdudeman@users.noreply.github.com>
2023-05-27 13:14:58 -0400

    human player

--- i_human_player_impl.py
+++ i_human_player_impl.py
@@ -8,16 +8,36 @@ from i_chess_model_state import Piece
 
 class HumanPlayerImpl(IPlayer):
     promotionType = None
+    promotionPiece = None
 
-    def getNextMove(self, boardState: IChessModelState) -> IMove:
+    # Name:    getNextMove
+    # Purpose: Prompt user for location and destination spots and promotion
+    #          if applicable.
+    # Return:  a player IMove
+    def getNextMove(self, boardState : IChessModelState) -> IMove:
         fromSpot = input("From (e.g. \"e4\"): ")
         toSpot = input ("To (e.g. \"e5\"): ")
 
-        if (toSpot[1] == 8 or toSpot[1] == 1):
-            if (boardState.getPieceAtSpot(SpotImpl(fromSpot[0], fromSpot[1])) == Piece.PAWN):
-                promotionType = input("Promote pawn to: ")
+        # check if piece needs to be promoted
+        if (int(toSpot[1]) == 8 or int(toSpot[1]) == 1):
+            if (boardState.getPieceAtSpot(SpotImpl(fromSpot[0], int(fromSpot[1]))) == Piece.PAWN):
+                self.promotionType = input("Promote pawn to: ")
+            
+            if (self.promotionType[1].lower() == 'n'):
+                self.promotionPiece = Piece.KNIGHT
+            elif (self.promotionType[0].lower() == 'q'):
+                self.promotionPiece = Piece.QUEEN
+            elif (self.promotionType[0].lower() == 'b'):
+                self.promotionPiece = Piece.BISHOP
+            elif (self.promotionType[0].lower() == 'k'):
+                self.promotionPiece = Piece.KING
+            elif (self.promotionType[0].lower() == 'p'):
+                self.promotionPiece = Piece.PAWN
+            elif (self.promotionType[0].lower() == 'r'):
+                self.promotionPiece = Piece.ROOK
 
+        # store move with or without promotion type
         move = MoveImpl(SpotImpl(fromSpot[0], int(fromSpot[1])), 
-                        SpotImpl(toSpot[0], int(toSpot[1])))
+                SpotImpl(toSpot[0], int(toSpot[1])), self.promotionPiece)
 
         return move
--- test_i_human_player_impl.py
+++ test_i_human_player_impl.py
@@ -3,16 +3,23 @@ from spot_impl import SpotImpl
 from i_human_player_impl import HumanPlayerImpl
 from chess_model_impl import ChessModelImpl
 from i_chess_model_state import IChessModelState
+from ast import Assert
 
 class TestSpotImpl(unittest.TestCase):
 
     def setUp(self):
         self.chessModel = ChessModelImpl()
-        self.chessModelState = IChessModelState()
         self.player1 = HumanPlayerImpl()
         self.player2 = HumanPlayerImpl()
 
-    def testGetNextMove(self):
+    def testGetNextMove_0(self):
         print("\n")
-        move = self.player1.getNextMove(self.chessModelState)
-        print(move.getMoveAsString())
+        move = self.player1.getNextMove(self.chessModel)
+        self.assertEqual(move.getMoveAsString(), "a2a8")
+        self.assertEqual(move.getPromotionTypeIfAvailable().name, "KING")
+
+    def testGetNextMove_1(self):
+        print("\n")
+        move = self.player1.getNextMove(self.chessModel)
+        self.assertEqual(move.getMoveAsString(), "b2b1")
+        self.assertEqual(move.getPromotionTypeIfAvailable().name, "ROOK")
